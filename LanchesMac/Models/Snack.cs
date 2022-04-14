94090896a46011b65ce53e75b6d8f173918de729
Diogo Lessa <diogodslessa@gmail.com>
2022-04-14 16:05:49 -0300

    Objects(Snacks and Category), DbContext and connection string added

---
+++ LanchesMac/Models/Snack.cs
@@ -0,0 +1,18 @@
+﻿namespace LanchesMac.Models
+{
+    public class Snack
+    {
+        public int SnackId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string DetailedDescription { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsFavoriteSnack { get; set; }
+        public bool InStock { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }  
+    }
+}
