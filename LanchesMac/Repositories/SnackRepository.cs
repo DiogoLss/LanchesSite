7c86fbc901a8a162c159bb3d88e51ea754bfb225
Diogo Lessa <diogodslessa@gmail.com>
2022-04-26 19:25:56 -0300

    Category and Snack repositories created, and dependency injected

---
+++ LanchesMac/Repositories/SnackRepository.cs
@@ -0,0 +1,23 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class SnackRepository : ISnackRepository
+    {
+        private readonly AppDbContext _context;
+        public SnackRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Snack> Snacks => throw new NotImplementedException();
+
+        public IEnumerable<Snack> FavoriteSnacks => throw new NotImplementedException();
+
+        public Snack GetSnackById(int snackId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
