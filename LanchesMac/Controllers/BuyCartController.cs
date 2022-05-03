ac9ad889ad89b3879689b927ce23ad281717293b
Diogo Lessa <diogodslessa@gmail.com>
2022-05-03 14:44:41 -0300

    BuyCartController implemented

--- LanchesMac/Controllers/BuyCartController.cs
+++ LanchesMac/Controllers/BuyCartController.cs
@@ -1,5 +1,6 @@
 ﻿using LanchesMac.Models;
 using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -17,7 +18,33 @@ namespace LanchesMac.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var items = _buyCart.GetBuyItemsCart();
+            _buyCart.BuyItemsCart = items;
+
+            var buyCartVM = new BuyCartViewModel
+            {
+                BuyCart = _buyCart,
+                BuyCartTotal = _buyCart.GetBuyCartTotal()
+            };
+            return View(buyCartVM);
+        }
+        public IActionResult AddItemToCart(int snackId)
+        {
+            var selectedSnack = _snackRepository.Snacks.FirstOrDefault(x => x.SnackId == snackId);
+            if(selectedSnack != null)
+            {
+                _buyCart.AddToCart(selectedSnack);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult RemoveItemFromCart(int snackId)
+        {
+            var selectedSnack = _snackRepository.Snacks.FirstOrDefault(x => x.SnackId == snackId);
+            if (selectedSnack != null)
+            {
+                _buyCart.RemoveFromCart(selectedSnack);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ LanchesMac/ViewModels/BuyCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.ViewModels
+{
+    public class BuyCartViewModel
+    {
+        public BuyCart BuyCart { get; set; }
+        public double BuyCartTotal { get; set; }
+    }
+}
