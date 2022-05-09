b7cf9f82c7b4b6c8a830c3ee57d3ac3fc66a4ebc
Diogo Lessa <diogodslessa@gmail.com>
2022-05-09 14:54:08 -0300

    Search bar, Action Search created and implemented

--- LanchesMac/Controllers/SnackController.cs
+++ LanchesMac/Controllers/SnackController.cs
@@ -41,5 +41,29 @@ namespace LanchesMac.Controllers
             var snack = _snackRepository.Snacks.FirstOrDefault(s => s.SnackId == snackId);
             return View(snack);
         }
+        public IActionResult Search(string searchString)
+        {
+            IEnumerable<Snack> snacks;
+            string currentCategory = string.Empty;
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                snacks = _snackRepository.Snacks.OrderBy(s => s.SnackId);
+                currentCategory = "All snacks";
+            }
+            else
+            {
+                snacks = _snackRepository.Snacks
+                    .Where(s => s.Name.ToLower().Contains(searchString.ToLower()) 
+                    || s.ShortDescription.ToLower().Contains(searchString.ToLower()));
+                if (snacks.Any()) { currentCategory = "All results found by " + searchString; }
+                else { currentCategory = "No snacks were found by " + searchString; }
+            }
+            return View("~/Views/Snack/List.cshtml", new SnackListViewModel
+            {
+                Snacks=snacks,
+                CurrentCategory=currentCategory
+            });
+        }
     }
 }
