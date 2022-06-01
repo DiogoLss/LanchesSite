2de125b8d80f3606dc0396dcdb10c92d0c761765
Diogo Lessa <diogodslessa@gmail.com>
2022-05-31 21:33:54 -0300

    Yearly Graphic added

--- LanchesMac/Areas/Admin/Controllers/AdminGraphicController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminGraphicController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminGraphicController : Controller
     {
         private readonly SalesGraphicService _graphic;
