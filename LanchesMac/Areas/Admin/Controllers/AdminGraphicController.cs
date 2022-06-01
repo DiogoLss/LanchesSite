0a6f1978a824296c60dc222ed3ff80528c17444e
Diogo Lessa <diogodslessa@gmail.com>
2022-05-31 21:04:23 -0300

    dropdown Sales Graphics added

--- LanchesMac/Areas/Admin/Controllers/AdminGraphicController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminGraphicController.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanchesMac.Areas.Admin.services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Areas.Admin.Controllers
 {
     public class AdminGraphicController : Controller
     {
-        public IActionResult Index()
+        private readonly SalesGraphicService _graphic;
+
+        public AdminGraphicController(SalesGraphicService graphic)
+        {
+            _graphic = graphic;
+        }
+
+        public JsonResult SnacksSales(int days)
+        {
+            var snacksSalesTotal = _graphic.GetSnackSales(days);
+            return Json(snacksSalesTotal);
+        }
+        [HttpGet]
+        public IActionResult Index(int days)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MonthlySales(int days)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult WeeklySales(int days)
         {
             return View();
         }
